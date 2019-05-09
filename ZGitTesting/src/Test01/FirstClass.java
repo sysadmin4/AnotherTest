86c91250fdbbc2e5214fa43debfab163fe0bb583
nylonx <nylonx@Nylon>
2019-05-10 00:25:36 +0200

    hey

---
+++ ZGitTesting/src/Test01/FirstClass.java
@@ -0,0 +1,14 @@
+package Test01;
+
+public class FirstClass {
+	private String nom = "hola";
+	
+	public static void main (String[] args) {
+		
+		FirstClass item = new FirstClass();
+		
+		
+		System.out.println(item.nom);
+		System.out.println("bye");
+	}
+}
